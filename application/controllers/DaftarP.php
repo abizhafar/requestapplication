391af7249e86bf6e4b86246850e5d1ffc2eeb4fc
KDraxler <39791072+KDraxler@users.noreply.github.com>
2019-07-15 16:04:03 +0700

    Create DaftarP.php

---
+++ application/controllers/DaftarP.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class DaftarP extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('daftarP_view');
+	}
+
+}
+
+/* End of file DaftarP.php */
+/* Location: ./application/controllers/DaftarP.php */
