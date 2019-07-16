c0f2bbf246c82035f9dc14146589909474745c09
Hilda Lauda Satria <satriahilda3@gmail.com>
2019-07-16 11:21:46 +0700

    Divis

---
+++ application/views/Divisi_view.php
@@ -0,0 +1,42 @@
+<?php $this->view('template.php'); ?>
+
+	
+
+						<section class="panel">
+							<header class="panel-heading">
+								<div class="panel-actions">
+									<a href="#" class="fa fa-caret-down"></a>
+									<a href="#" class="fa fa-times"></a>
+								</div>
+						
+								<h2 class="panel-title">Table</h2>
+							</header>
+							<div class="panel-body">
+								<table class="table table-bordered table-striped mb-none" id="datatable-default">
+									<thead>
+										<tr>
+											<th>Kode Divisi</th>
+											<th>Nama Divisi</th>
+											
+										</tr>
+									</thead>
+									<tbody>
+										<tr class="gradeX">
+											<td>Trident</td>
+											<td>Internet
+												Explorer 4.0
+											</td>
+											
+							</div>
+						</section>
+						
+			
+						</div>
+					</div>
+				</div>
+			</aside>
+		</section>
+
+	<?php $this->view('footer.php'); ?>
+
+	</body>
