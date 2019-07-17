cb75300112136b2daa03e01075a464bc8f6a5293
KDraxler <39791072+KDraxler@users.noreply.github.com>
2019-07-17 16:32:42 +0700

    Create Registrasi.php

---
+++ application/controllers/Registrasi.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Registrasi extends CI_Controller {
+
+	public function index()
+	{
+		$this->load->view('registrasi_view');
+	}
+
+}
+
+/* End of file Registrasi.php */
+/* Location: ./application/controllers/Registrasi.php */
