394abaed0bf4b2514048e0787d38867857df230e
abizhafar <abizhafar@gmail.com>
2019-07-18 16:33:53 +0700

    update user

--- application/models/User_Model.php
+++ application/models/User_Model.php
@@ -22,8 +22,9 @@
     		$this->datatables->select('id_user,nama_user,username,password,akses');
     		$this->datatables->from('user');
     		$this->datatables->add_column('view',
-    		'<a href="javascript:void(0);" class="edit_record btn btn-warning btn-s" data-id_user="$1" data-nama_user="$2" data-username="$3" data-password="$4" data-akses="$5"><i class="fa fa-edit"></i> EDIT</a>
-    		 <a href="javascript:void(0);" class="hapus_record btn btn-danger btn-s" data-id_user="$1"><i class="glyphicon glyphicon-remove-sign"></i> HAPUS</a>', 'id_user,nama_user,username,password,akses');
+    		'<a href="javascript:void(0);" class="edit_record" data-id_user="$1" data-nama_user="$2" data-username="$3" data-password="$4" data-akses="$5"><i style="font-size:20px" class="icon fa fa-edit"></i></a>
+    		&nbsp &nbsp &nbsp
+    		 <a href="javascript:void(0);" class="hapus_record" data-id_user="$1"><i style="font-size:15px" class="glyphicon glyphicon-trash"></i></a>', 'id_user,nama_user,username,password,akses');
         
     		return $this->datatables->generate();
  		}
