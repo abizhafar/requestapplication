207134f7c0937c3bfb9b2b4cf73d9897ada7e99b
abizhafar <abizhafar@gmail.com>
2019-07-18 10:56:56 +0700

    update user n template bug

--- application/views/home.php
+++ application/views/home.php
@@ -171,7 +171,7 @@
       </div>
       <div class="row">
         <div class="col-md-12">
-          <p class="text-muted">© Copyright 2019 PT. PAL INDONESIA - DIVISI TENOLOGI</p>
+          <p class="text-muted">© Copyright 2019 PT. PAL INDONESIA - DIVISI TEKNOLOGI </p> 
         </div>
       </div>
     </div>
