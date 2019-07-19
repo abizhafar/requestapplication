8624b8baed695e09991d98c309779a5d377043d4
Hilda Lauda Satria <satriahilda3@gmail.com>
2019-07-19 10:09:09 +0700

    edit divisi dan menghapus departemen

--- application/models/Divisi_model.php
+++ application/models/Divisi_model.php
@@ -3,12 +3,12 @@ class Divisi_model extends CI_Model{
 
   function get_all_vm() 
   {
-    $this->datatables->select('kd_divisi,nama_divisi');
+    $this->datatables->select('id_divisi,divisi');
     $this->datatables->from('divisi');
     $this->datatables->add_column('view',
-    '<a href="javascript:void(0);" class="edit_record btn btn-warning btn-s" data-kd_divisi="$1" data-nama_divisi="$2"><i class="fa fa-edit"></i> &nbsp EDIT </a>
-    <a href="javascript:void(0);" class="hapus_record btn btn-danger btn-s" data-kd_divisi="$1"><i class="fa fa-trash-o"></i> &nbsp Hapus</a>',
-    'kd_divisi,nama_divisi');
+    '<a href="javascript:void(0);" class="edit_record btn btn-warning btn-s" data-id_divisi="$1" data-divisi="$2"><i class="fa fa-edit"></i> &nbsp EDIT </a>
+    <a href="javascript:void(0);" class="hapus_record btn btn-danger btn-s" data-id_divisi="$1"><i class="fa fa-trash-o"></i> &nbsp Hapus</a>',
+    'id_divisi,divisi');
         
     return $this->datatables->generate();
   }
--- application/views/Divisi_view.php
+++ application/views/Divisi_view.php
@@ -12,12 +12,11 @@
 								<h2 class="panel-title">Table</h2>
 							</header>
 							<div class="panel-body">
-								<table class="table table-bordered table-striped mb-none" id="datatable-default">
-									
-										 <div class="card-body card-block">
-          						<button class="btn btn-success" data-toggle="modal" data-target="#myModalAdd" onclick="delrec()">Tambah Divisi</button>
-          					</div>
-          					<br>
+										<div class="table-responsive">
+											<button class="btn-md btn-outline-success" data-toggle="modal" data-target="#myModalAdd" class onclick="delrec()"><i class="glyphicon glyphicon-plus"></i> TAMBAH USER</button>
+										 	<br><br>
+							<div class="panel-body">
+								<table class="display" id="mytable"  style="width:100%">
           							<thead>
 										<tr>
 											<th>Kode Divisi</th>
@@ -26,26 +25,220 @@
 											
 										</tr>
 									</thead>
-									<tbody>
-										<tr class="gradeX">
-											<td>Trident</td>
-											<td>Internet
-												Explorer 4.0
-												<td class="actions">
-															<a href=""><i class="fa fa-pencil"></i></a>
-															<a href="" class="delete-row"><i class="fa fa-trash-o"></i></a>
-											</td>
-											
+									</table>
 							</div>
 						</section>
-						
-			
+						<form id="add-row-form" action="<?php echo base_url().'index.php/Divisi/simpan'?>" method="post">
+             <div class="modal fade" id="myModalAdd" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                   <div class="modal-content">
+                       <div class="modal-header">
+                           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                           <h4 class="modal-title" id="myModalLabel">TAMBAH DIVISI</h4>
+                       </div>
+                       <div class="alert alert-danger print-error-msg" style="display:none"></div>
+                       <div class="alert alert-primary print-success-msg" style="display:none"></div>
+                       <div class="modal-body">
+                           <div class="form-group">
+                               <input type="text" id="id_divisi" name="Kode" class="form-control" placeholder="Masukkan Kode Divisi" required>
+                           </div>
+                           <div class="form-group">
+                               <input type="text" id="Nama_divisi" name="Nama" class="form-control" placeholder=" Masukkan Nama Divisi" required>
+                           </div>
+                           
+			 </div>
+                     <div class="modal-footer">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">TUTUP</button>
+                          <button type="submit" id="add-row" class="btn btn-success"><i class="glyphicon glyphicon-floppy-save"></i> SIMPAN</button>
 						</div>
 					</div>
 				</div>
 			</aside>
 		</section>
+	</div>
+</form>
+<form id="add-row-form" action="<?php echo base_url().'index.php/Divisi/update'?>" method="post">
+           <div class="modal fade" id="ModalUpdate" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+              <div class="modal-dialog">
+                 <div class="modal-content">
+                     <div class="modal-header">
+                         <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                         <h4 class="modal-title" id="myModalLabel">UPDATE DIVISI</h4>
+                     </div>
+                     <div class="modal-body">
+                         <div class="form-group">
+                               <input type="text" name="id" class="form-control" placeholder="Masukkan Kode Divisi" readonly>
+                           </div>
+                           <div class="form-group">
+                               <input type="text" name="Nama" class="form-control" placeholder=" Masukkan Nama Divisi" required>
+                           </div>
+                           
+                     </div>
+                     <div class="modal-footer">
+                          <button type="button" class="btn btn-default" data-dismiss="modal">TUTUP</button>
+                          <button type="submit" id="add-row" class="btn btn-info"><i class="glyphicon glyphicon-edit"></i> PERBARUI</button>
+                     </div>
+                  </div>
+              </div>
+           </div>
+       </form>
+		
+		<!-- Modul hapus data -->
+       <form id="dell-row-form" action="<?php echo site_url().'/Divisi/delete'?>" method="post">
+       <div class="modal fade" id="ModalHapus" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+          <div class="modal-dialog">
+             <div class="modal-content">
+                 <div class="modal-header">
+                     <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                     <h4 class="modal-title" id="myModalLabel">Hapus Divisi</h4>
+                 </div>
+                 <div class="modal-body">
+                         <input type="button" id="del-divisi" name="Nomer" class="form-control" placeholder="Nomer Petugas" required>
+                         <strong>Anda yakin ingin menghapus record ini?</strong>
+                 </div>
+                 <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">TUTUP</button>
+                      <button type="submit" id="dell-row" class="btn btn-danger"><i class="glyphicon glyphicon-remove-sign"></i> HAPUS</button>
+                 </div>
+              </div>
+          </div>
+        </div>
+      </form>
 
 	<?php $this->view('footer.php'); ?>
 
 	</body>
+
+<script>
+	function delrec() {
+  document.getElementById("add-row-form").reset();
+}
+  $(document).ready(function(){
+    // Setup datatables
+    $.fn.dataTableExt.oApi.fnPagingInfo = function(oSettings)
+      {
+          return {
+              "iStart": oSettings._iDisplayStart,
+              "iEnd": oSettings.fnDisplayEnd(),
+              "iLength": oSettings._iDisplayLength,
+              "iTotal": oSettings.fnRecordsTotal(),
+              "iFilteredTotal": oSettings.fnRecordsDisplay(),
+              "iPage": Math.ceil(oSettings._iDisplayStart / oSettings._iDisplayLength),
+              "iTotalPages": Math.ceil(oSettings.fnRecordsDisplay() / oSettings._iDisplayLength)
+          };
+      };
+
+      var table = $("#mytable").DataTable({
+          initComplete: function() {
+              var api = this.api();
+              $('#mytable_filter input')
+                  .off('.DT')
+                  .on('input.DT', function() {
+                      api.search(this.value).draw();
+              });
+          },	
+              oLanguage: {
+              sProcessing: "LOADING..."
+          },
+              processing: true,
+              serverSide: true,
+              ajax: {"url": "<?php echo base_url().'index.php/Divisi/get_guest_json'?>", "type": "POST"},
+                  columns: [
+                        {"data": "id_divisi"},
+                        {"data": "divisi"},
+                        {"data": "view", "sortable":false},
+                        // {"data": "view"},
+                  ],
+              order: [[1, 'asc']],
+          rowCallback: function(row, data, iDisplayIndex) {
+              var info = this.fnPagingInfo();
+              var page = info.iPage;
+              var length = info.iLength;
+              $('td:eq(0)', row).html();
+          }
+
+      });
+      // end setup datatables
+      // get Edit Records
+      $('#mytable').on('click','.edit_record',function(){
+            var id=$(this).data('id_divisi');
+            var Nama=$(this).data('divisi');
+           
+            // var akses=$(this).data("akses");
+            $('#ModalUpdate').modal('show');
+            $('[name="id"]').val(id);
+            $('[name="Nama"]').val(Nama);
+            
+      });
+      // End Edit Records
+      // get Hapus Records
+      $('#mytable').on('click','.hapus_record',function(){
+            var id=$(this).data('id_divisi');
+            $('#ModalHapus').modal('show');
+            $('[name="Nomer"]').val(id);
+      });
+      // End Hapus Records
+
+  });
+</script>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+      $("#add-row").click(function(e){
+        e.preventDefault();
+        var id_divisi = $("input[id='id_divisi']").val();
+        var nama_divisi = $("input[id='Nama_divisi']").val();
+       
+        // var hak_akses =$("radio[id='Radio']").val();
+          $.ajax({
+              url: "<?php echo site_url() ?>/Divisi/simpan",
+              type:'POST',
+              dataType: "json",
+              data: {Kode:id_divisi, Nama:nama_divisi}, 
+              success: function(data) {
+                  if($.isEmptyObject(data.error)){
+                    $(".print-success-msg").css('display','block');
+                    // alert(data.success);
+                    $(".print-success-msg").html(data.success);
+                    $(".print-error-msg").css('display','none');
+
+                    location.replace("<?php echo site_url() ?>/Divisi");
+                  }else{
+                    $(".print-error-msg").css('display','block');
+                    $(".print-error-msg").html(data.error);
+                  }
+              }
+          });
+      }); 
+  });
+</script>
+
+<script type="text/javascript">
+  $(document).ready(function() {
+      $("#dell-row").click(function(e){
+        e.preventDefault();
+        var id = $("input[id='del-divisi']").val();
+          $.ajax({
+              url: "<?php echo site_url() ?>/Divisi/delete",
+              type:'POST',
+              dataType: "json",
+              data: {Nomer:id},
+              success: function(data) {
+                  if($.isEmptyObject(data.error)){
+                    $(".print-success-msg").css('display','block');
+                    // alert(data.success);
+                    $(".print-success-msg").html(data.success);
+                    $(".print-error-msg").css('display','none');
+
+                    location.replace("<?php echo site_url() ?>/Divisi");
+                  }
+                  // else{
+                  //   $(".print-error-msg").css('display','none');
+                  //   alert(data.error);
+                  //   // $(".print-error-msg").html(data.error);
+                  // }
+              }
+          });
+      }); 
+  });
+</script>
