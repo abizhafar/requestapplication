8e034219369970ca518a9211e42330937a066e1c
Eko Prasetyo Kurniawan <ekop22496@gmail.com>
2019-07-19 13:57:53 +0700

    Update registrasi_view.php

--- application/views/registrasi_view.php
+++ application/views/registrasi_view.php
@@ -241,8 +241,10 @@
 		</div>
 	</div>
 	<!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script> -->
-	<!-- <script src="<?php echo base_url() ?>assets/vendor/bootrap/js/bootstrap.js"></script> -->
+	<script src="<?php echo base_url() ?>assets/vendor/bootrap/js/bootstrap.js"></script>
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+	
 
 	<!-- <script src="<?php echo base_url() ?>assets/js/jquery-3.3.1.min.js"></script> -->
 	<script src="<?php echo base_url() ?>assets/js/jquery.steps.js"></script>
@@ -254,7 +256,6 @@
 	<script src="<?php echo base_url() ?>assets/vendor/bootstrap-markdown/js/to-markdown.js"></script>
 	<script src="<?php echo base_url() ?>assets/vendor/bootstrap-markdown/js/bs-md.js"></script>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 
 
 <script type="text/javascript">
